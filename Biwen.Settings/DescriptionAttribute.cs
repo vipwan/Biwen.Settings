1dfd2de8c74219442a069abffb2b7fa642b1820d
万雅虎 <vipwan@outlook.com>
2023-08-30 15:28:28 +0800

    描述信息

--- Biwen.Settings/DescriptionAttribute.cs
+++ Biwen.Settings/DescriptionAttribute.cs
@@ -5,8 +5,11 @@
     /// 描述信息 用于Setting,和Setting的属性
     /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-    public class DescriptionAttribute : Attribute
+    public sealed class DescriptionAttribute : Attribute
     {
+        /// <summary>
+        /// 描述信息
+        /// </summary>
         public string? Description { get; set; }
         public DescriptionAttribute(string? description)
         {
